01a47f31f973e2ea7c6c65e1d22b8556c7b60632
unknown <mikus.meimanis.metta@inbox.lv>
2021-12-02 23:18:47 +0200

    final bez runam

--- main.cpp
+++ main.cpp
@@ -13,8 +13,11 @@ main ()
   using namespace std::this_thread; 
   using namespace std::chrono; 
 
+  // Roles
   const char *lane[5] =
   {"Top", "Jungle", "Mid", "Support", "Bottom"};
+  
+  // Čempioni
   const char *champion[156] =
   { "Aatrox", "Ahri", "Akali", "Akshan", "Alistar", "Amumu", "Anivia",
 "Annie", "Aphelios", "Ashe", "Aurelion Sol", "Azir", "Bard", "Blitzcrank", "Brand", "Braum",
@@ -33,6 +36,8 @@ main ()
 "Twisted Fate", "Udyr", "Urgot", "Varus", "Vayne", "Veigar", "Vel'Koz", "Vex", "Vi", "Viego",
 "Viktor", "Vladimir", "Volibear", "Warwick", "Wukong", "Xayah", "Xerath", "Xin Zhao", "Yasuo",
 "Yone", "Yorick", "Yuumi", "Zac", "Zed", "Ziggs", "Zilean", "Zoe", "Zyra" };
+  
+  // Mythic itemi
   const char *mythic[25] =
   { "Crown of the Shattered Queen", "Divine Sunderer",
 "Duskblade of Draktharr", "Eclipse", "Evenshroud", "Everfrost", "Frostfire Gauntlet",
@@ -41,6 +46,8 @@ main ()
 "Moonstone Renewer", "Night Harvester", "Prowler 's Claw", "Riftmaker",
 "Shurelya' s Battlesong ", "Stridebreaker ", "Sunfire Aegis ", "Trinity Force ",
 "Turbo Chemtank" };
+  
+  // Legendary itemi
   const char *legendary[72] =
     { "Abyssal Mask", "Anathemas Chains", "Archangel's staff",
 "Ardent Censer ", "Axiom Arc", "Banshee's Veil", "Black Cleaver", "Black Mist Scythe",
@@ -58,10 +65,86 @@ main ()
 "Sterak's gage", "Stormrazor", "The Collector", "Thornmail", "Titanic Hydra", "Umbral Glaive",
 "Vigilant Wardstone", "Void Staff", "Warmog's Armor", "Winter's Approach", "Wit's End",
 "Youmuu's Ghostblade", "Zeke's Convergence", "Zhonya's Hourglass" };
+  // Apavi
+  
   const char *boots[7] =
   { "Berserker's Greaves", "Boots of Swiftness", "Ionian Boots of Lucidity",
 "Mercury's Treads", "Mobility Boots", "Plated Steelcaps", "Sorcerer's Shoes" };
+  
+  /*Runes path
+  const char *primary[5] = 
+  {"Domination", "Precision", "Sorcery", "Resolve", "Inspiration"};
 
+  // Domination path
+  const char *Deystone[4] =
+  {"Electrocute", "Predator", "Dark Harvest", "Hail of Blades"};
+  
+  const char *Dslot1[3] = 
+  {"Cheap Shot", "Taste of Blood", "Sudden Impact"};
+  
+  const char *Dslot2[3] = 
+  {"Zombie Ward", "Ghost Poro", "Eyeball Collection"};
+  
+  const char *Dslot3[4] = 
+  {"Ravenous Hunter", "Ingenious Hunter", "Relentless Hunter", "Ultimate Hunter"};
+  
+  
+  // Precision path
+  const char *Peystone[4] = 
+  {"Conqueror", "Press the Attack", "Lethal Tempo", "Fleet Footwork"};
+  
+  const char *Pslot1[3] = 
+  {"Pressence of Mind", "Triumph", "Overheal"};
+  
+  const char *Pslot2[3] = 
+  {"Legend: Alacrity", "Legend: Tenacity", "Legend: Bloodline"};
+  
+  const char *Pslot3[3] = 
+  {"Coup De Grace", "Cut Down", "Last Stand"};
+  
+  
+  // Sorcery path
+  const char *Seystone[3] = 
+  {"Summon Aery", "Arcane Comet", "Phase Rush"};
+  
+  const char *Sslot1[3] = 
+  {"Nullifying Orb", "Manaflow Band", "Nimbus Cloak"};
+  
+  const char *Sslot2[3] = 
+  {"Transendence", "Celerity", "Absolute Focus"};
+  
+  const char *Sslot3[3] = 
+  {"Schorch", "Waterwalking", "Gathering Storm"};
+  
+  
+  // Resolve path
+  const char *Reystone[3] = 
+  {"Grasp of the Undying", "Aftershock", "Guardian"};
+  
+  const char *Rslot1[3] = 
+  {"Demolish", "Font of Life", "Shield Bash"};
+  
+  const char *Rslot2[3] = 
+  {"Conditioning", "Bone Plating", "Second Wind"};
+  
+  const char *Rslot3[3] = 
+  {"Unflinching", "Revitalize", "Overgrowth"};
+  
+  
+  // Inspiration path
+  const char *Ieystone[3] = 
+  {"Glacial Augument", "Unsealed Spellbook", "First Strike"};
+  
+  const char *Islot1[3] = 
+  {"Hextech Flashtraption", "Magical Footwear", "Perfect Timing"};
+  
+  const char *Islot2[3] = 
+  {"Future's Market", "Minion Dematerializer", "Biscuit Delivery"};
+  
+  const char *Islot3[3] = 
+  {"Cosmic Insight", "Approach Velocity", "Time Warp Tonic"};*/
+  
+ 
   int champ;
   srand(time(NULL));
   champ = rand () % 156 + 1;
@@ -75,7 +158,7 @@ main ()
   std::cout << lane[role] << std::endl;
   sleep_for(nanoseconds(1));
   sleep_until(system_clock::now() + seconds(1));
-
+  
   int myth;
   srand(time(NULL));
   myth = rand () % 24 + 1;
